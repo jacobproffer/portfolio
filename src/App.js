f3561d3e37c31ed14498bd6319e9a22d8cc2cf64
jacobproffer <jacobhardingproffer@gmail.com>
2019-06-27 12:14:54 -0400

    Remove ruby

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import GlobalStyles from './styles/global-styles';
 import Header from './components/Header';
 import Hero from './components/home/Hero';
@@ -7,10 +6,6 @@ import Intro from './components/home/Intro';
 import Sites from './components/home/Sites';
 import Footer from './components/Footer';
 import image from './static/hero.jpg';
-
-const Ruby = styled.ruby`
-  ruby-align: start;
-`;
 
 function App() {
   return (
@@ -25,9 +20,9 @@ function App() {
         <Intro
           heading="インフォ"
         >
-          <p><Ruby>私<rt>わたし</rt></Ruby>はアッパーミシガン<Ruby>州<rt>しゅう</rt></Ruby>のウェブ開発者です。</p>
+          <p>私はアッパーミシガン州のウェブ開発者です。</p>
         </Intro>
-        <Sites />
+        <Sites heading="作品" />
       </main>
       <Footer />
     </React.Fragment>
